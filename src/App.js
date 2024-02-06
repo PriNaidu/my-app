ae282e88fd0307c044a692be4734eb94ff243b8b
PriNaidu <npriyanka783@gmail.com>
2024-02-06 17:45:37 +0530

    [Changes]

--- src/App.js
+++ src/App.js
@@ -6,35 +6,12 @@ import Counter from "./Components/Counter";
 import Users from "./Components/Users";
 import Carousel from "./Components/Carousel";
 import CustomCarousel from "./Components/CustomCarousel";
-import DragAndDrop from "./Components/DragAndDrop"
+// import DragAndDrop from "./Components/DragAndDrop"
 function App() {
   return (
     <div className="App">
-      <DragAndDrop />
-      {/* <CustomCarousel /> */}
-      {/* <Users/> */}
-      {/* <Counter/>
-      <Message/> */}
-      {/* <h1>Functional Components example</h1> 
-      <Greet name="Priyanka" heroName="Wonder WoMan">
-        <p>Hello this is priyanka.</p>
-      </Greet>
-      <Greet name="Sarvesh" heroName="Bat Man">
-        <button>Click me</button>
-      </Greet>
-      <Greet name="Aniket" heroName="Rod Man">
-        <div>
-          <p>This is a paragraph</p>
-          <span>Span tooo</span>
-          <button>And a button also</button>
-        </div>
-      </Greet> */}
-      {/* <h1>Class Components examples</h1>
-      <Class name="Priyanka" heroName="Wonder WoMan">
-        Hello
-      </Class>
-      <Class name="Sarvesh" heroName="Iron Man" />
-      <Class name="Aniket" heroName="Pagal Man"/> */}
+      {/* <DragAndDrop /> */}
+     <Greet/>
 
     </div>
   );
